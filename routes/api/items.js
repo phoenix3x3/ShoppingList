f51fb562dacc25bb578d9deee15481fbcddf6430
phoenix3x3 <fenix3x3@mail.ru>
2019-11-15 21:30:12 +0300

    add base of the client and possibility to start both clients concurrency

--- routes/api/items.js
+++ routes/api/items.js
@@ -34,9 +34,7 @@ router.delete('/:id', (req, res) => {
     Item.findById(req.params.id)
         .then(item => item.remove().then(() => res.json({success: true})))
         .catch(err => res.status(404).json({success: false}));
-        
 });
-
 
 
 module.exports = router;
