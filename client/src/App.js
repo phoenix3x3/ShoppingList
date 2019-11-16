8ac92ad1b556b9f2288a51598316df52cea898d1
phoenix3x3 <fenix3x3@mail.ru>
2019-11-16 22:02:25 +0300

    add shoping list

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AppNavbar from './components/AppNavbar'
-// import ShopingList from './components/ShoppingList'
+import ShopingList from './components/ShoppingList'
 function App() {
   return (
     <div className="App">
       <AppNavbar />
-      {/* <ShopingList /> */}
+      <ShopingList />
     </div>
   );
 }
