f9e0b6511cc9dca53295eb5f6004998d69b1b324
phoenix3x3 <fenix3x3@mail.ru>
2019-11-20 05:10:02 +0300

    connect front-end & back-end

--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -10,8 +10,8 @@ class ShoppingList extends React.Component {
     this.props.getItems();
   }
 
-  onDeleteClick = id => {
-    this.props.deleteItem(id);
+  onDeleteClick = _id => {
+    this.props.deleteItem(_id);
   };
 
   render() {
@@ -20,14 +20,14 @@ class ShoppingList extends React.Component {
       <Container>
         <ListGroup>
           <TransitionGroup className="shopping-list">
-            {items.map(({ id, name }) => (
-              <CSSTransition key={id} timeout={500} classNames="fade">
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
                   <Button
                     className="remove-btn"
                     color="danger"
                     size="sm"
-                    onClick={this.onDeleteClick.bind(this, id)}
+                    onClick={this.onDeleteClick.bind(this, _id)}
                   >
                     &times;
                   </Button>
