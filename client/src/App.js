bbb4b6ff78c4e8178f6f1099c16a041e5f7acb82
phoenix3x3 <fenix3x3@mail.ru>
2019-11-24 00:03:33 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppNavbar from "./components/AppNavbar";
+import ShopingList from "./components/ShoppingList";
+import { Provider } from "react-redux";
+import store from "./store";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Container>
+          <ItemModal />
+          <ShopingList />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 
 const items = require("./routes/api/items");
 const app = express();
-const path = require("path");
 
 //Bodyparser Middleware
 app.use(bodyParser.json());
@@ -20,17 +19,6 @@ mongoose
   .catch(err => console.log(err));
 // Use Routes
 app.use("/api/items", items);
-
-// Serve  static assets if in production
-
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
