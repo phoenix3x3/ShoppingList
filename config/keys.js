d07d97499df26ba9362a30f7d45dbccb8e528078
phoenix3x3 <fenix3x3@mail.ru>
2019-12-29 09:19:41 +0300

    Version 1.0.0

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://andrew:199309QWErty@cluster0-zc3tt.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://abAlex:adKirillov@sunmait-qqwv5.mongodb.net/test?retryWrites=true&w=majority"
 };
+// mongodb+srv://andrew:199309QWErty@cluster0-zc3tt.mongodb.net/test?retryWrites=true&w=majority
