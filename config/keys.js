6dfc6df3ce9d675e1437afe3018aca1ae9bde6f2
phoenix3x3 <fenix3x3@mail.ru>
2020-01-02 23:27:46 +0300

    test

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://abAlex:adKirillov@sunmait-qqwv5.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://andrew:199309QWErty@cluster0-zc3tt.mongodb.net/test?retryWrites=true&w=majority"
 };
-// mongodb+srv://andrew:199309QWErty@cluster0-zc3tt.mongodb.net/test?retryWrites=true&w=majority
